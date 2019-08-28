6dd97dfbfcf9b07dc1b456951f0c3ad9e720855c
theone <xuguangyue@dtrobo.com>
2019-08-28 15:11:27 +0800

    TodoList element add

--- demo01/src/TodoList.js
+++ demo01/src/TodoList.js
@@ -1,15 +1,34 @@
 import React, { Component } from 'react'
+import 'antd/dist/antd.css'
+import { Input, Button, List } from 'antd'
+
+const data = [
+    'hahahah',
+    '呵呵呵呵',
+    'bbb'
+]
 
 class TodoList extends Component {
-    constructor(props) {
-        super(props)
-        this.state = {  }
-    }
-    render() { 
-        return ( 
-            <div>aaaa</div>
-         )
+    render() {
+        return (
+            <div style= {{ margin: '10px' }}>
+                <div>
+                    <Input
+                        placeholder="请输入"
+                        style={{ width: '250px', marginRight: '10px' }}
+                    />
+                    <Button type="primary">ADD</Button>
+                </div>
+                <div style={{ margin: '10px', width: '300px' }}>
+                    <List
+                        bordered
+                        dataSource={data}
+                        renderItem={item => (<List.Item>{item}</List.Item>)}
+                    />
+                </div>
+            </div>
+        )
     }
 }
- 
+
 export default TodoList
